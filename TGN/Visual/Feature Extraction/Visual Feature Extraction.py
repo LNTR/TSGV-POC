ec374c619a3b7c9f1b04ad51dbfe370040db27b8
Charith Lakshan <wgclakshan@gmail.com>
2025-10-26 12:45:55 +0530

    VGG16

--- TGN/Visual/Feature Extraction/Visual Feature Extraction.py
+++ TGN/Visual/Feature Extraction/Visual Feature Extraction.py
@@ -1,8 +1,29 @@
 import os
 from torchvision import transforms
-from Models.CNN_Encoder import VGG16
 import numpy as np
 import torch
+import torch.nn as nn
+
+class VGG16(nn.Module):
+    def __init__(self):
+        super(VGG16, self).__init__()
+        self.model = vgg16(pretrained=True, progress=True)
+        features = list(self.model.classifier.children())[:-1]  # removing the last layer
+        self.model.classifier = nn.Sequential(*features)
+
+        # freeze the weights of the network so it will not be trained
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+    def forward(self, x: torch.Tensor):
+        """
+        :param x: A bunch of frames as a torch.Tensor with shape (K, 224, 224, 3)
+        :return: a torch.Tensor containing the extracted features with shape (K, 4096)
+        """
+        self.model.eval()
+        return self.model(x)
+        #return torch.zeros([x.shape[0], 4096])
+
 
 def extract_visual_features(frames_path: str, features_path: str):
     """Extracts the features from frames using the pretrained VGG 16 network"""
